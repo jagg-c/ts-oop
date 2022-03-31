0d444bd4694ae6a90329b54d98cf09a5785a1f1e
Jhony Alexander Garcia Gómez <pojgarcia@postobon.com.co>
2022-03-31 15:31:04 -0500

    Create set for month with validation

---
+++ src/07-setters.ts
@@ -0,0 +1,58 @@
+export class MyDate {
+  constructor(
+    public year: number = 1990,
+    private _month: number = 1,
+    private _day: number = 1
+  ) {}
+
+  printFormat(): string {
+    const day = this.addPadding(this._day);
+    const month = this.addPadding(this._month);
+    return `${day}/${month}/${this.year}`;
+  }
+
+  private addPadding(value: number): string {
+    if (value < 10) {
+      return `0${value}`;
+    }
+    return `${value}`;
+  }
+
+  add(amount: number, type: 'days' | 'months' | 'years') {
+    if (type === 'days') {
+      this._day += amount;
+    }
+    if (type === 'months') {
+      this._month += amount;
+    }
+    if (type === 'years') {
+      this.year += amount;
+    }
+  }
+
+  get day() {
+    return this._day;
+  }
+
+  get isLeapYear(): boolean {
+    if (this.year % 400 === 0) return true;
+    if (this.year % 400 === 0) return false;
+    return this.year % 4 === 0;
+  }
+
+  get month() {
+    return this._month;
+  }
+
+  set month(newValue: number) {
+    if (newValue >= 1 && newValue <= 12) {
+      this._month = newValue;
+    } else {
+      throw new Error('month out of range');
+    }
+  }
+}
+
+const myDate = new MyDate(1993, 6, 3);
+console.log('2012', myDate.isLeapYear);
+myDate.month = 14
