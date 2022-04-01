2bb756afb7c30f638ba3340d01bc5c8f2f537eb2
Jhony Alexander Garcia Gómez <pojgarcia@postobon.com.co>
2022-04-01 07:05:11 -0500

    Create static properties and static method

---
+++ src/10-static.ts
@@ -0,0 +1,13 @@
+console.log('Math.PI', Math.PI);
+console.log('Math.max', Math.max(1, 2, 8, 3, 5, 4));
+
+export class MyMath {
+  static readonly PI = 3.14;
+
+  static max(...number: number[]) {
+    return number.reduce((max, item) => max >= item ? max: item, 0)
+  }
+}
+
+console.log('MyMath.PI:', MyMath.PI);
+console.log('MyMath.max:', MyMath.max(5, 1, 500, 2));
