2bc9d757933c4d17b15e2376bba243376358b0ab
Jhony Alexander Garcia Gómez <pojgarcia@postobon.com.co>
2022-04-12 01:12:06 -0500

    Interface Dirver for DB

---
+++ src/11-interfaces.ts
@@ -0,0 +1,44 @@
+export interface Driver {
+  database: string;
+  password: string;
+  port: number;
+
+  connect(): void;
+  disconnect(): void;
+  isConnected(name: string): boolean;
+}
+
+class PostgresDriver implements Driver {
+  constructor(
+    public database: string,
+    public password: string,
+    public port: number
+  ) {}
+
+  isConnected(name: string): boolean {
+    throw new Error('Method not implemented.');
+  }
+
+  connect(): void {}
+  disconnect(): void {}
+}
+
+class OracleDriver implements Driver {
+  constructor(
+    public database: string,
+    public password: string,
+    public port: number
+  ) {}
+
+  isConnected(name: string): boolean {
+    throw new Error('Method not implemented.');
+  }
+
+  disconnect(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  connect(): void {
+    throw new Error('Method not implemented.');
+  }
+}
