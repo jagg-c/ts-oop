cc90acb00f18e5d1be501ea44561579bf005efbf
Jhony Alexander Garcia Gómez <pojgarcia@postobon.com.co>
2022-04-12 01:20:44 -0500

    Abstract classes

---
+++ src/12-abstract.ts
@@ -0,0 +1,7 @@
+import { Animal, Dog } from './09-protected';
+
+// const animal = new Animal('elite'); // Can't implement super class, only subclasses
+// animal.greeting();
+
+const cheis = new Dog('cheis', 'nico');
+cheis.move();
